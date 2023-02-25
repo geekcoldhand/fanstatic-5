b4d2a036280b07f217c1cb882fc3647be2545203
Sefora Rodriguez <seforarodriguez13@gmail.com>
2023-02-25 16:34:01 -0500

    added code for viewmodel

--- app/src/main/java/com/tmo/syncupkids/app/fantastic5/FirstFragment.java
+++ app/src/main/java/com/tmo/syncupkids/app/fantastic5/FirstFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.tmo.syncupkids.app.fantastic5.databinding.FragmentFirstBinding;
@@ -14,6 +16,7 @@ import com.tmo.syncupkids.app.fantastic5.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    private FirstViewModel viewModel;
 
     @Override
     public View onCreateView(
@@ -21,6 +24,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
+        viewModel = new ViewModelProvider(this).get(FirstViewModel.class);
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -32,8 +36,10 @@ public class FirstFragment extends Fragment {
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                viewModel.rewardsPoints++;
+                binding.textView2.setText(String.valueOf(viewModel.rewardsPoints));
             }
         });
     }
